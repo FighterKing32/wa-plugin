166f2294da144e7af403306d02509aba87043c31
FighterKing32 <77884587+FighterKing32@users.noreply.github.com>
2021-02-06 23:58:11 +0530

    Update Waifu.js

--- Waifu.js
+++ Waifu.js
@@ -2,7 +2,17 @@ const Asena = require ('../ events');
 const {MessageType} = require ('@ adiwajshing / baileys');
 const got = require("got")
 
-Asena.addCommand ({pattern: 'waifu', fromMe: true, onlygroup: true}, (async (message, match) => {
+Asena.addCommand ({pattern: 'waifu ?(.*)', fromMe: true, onlygroup: true}, (async (message, match) => {
+    if (match[1] === "") {
+        try{
+            //const resp = await fetch("https://mywaifulist.moe-api.herokuapp.com/all").then(r => r.json());
+            const respo = await got("https://mywaifulist.moe-api.herokuapp.com/all").then(async ok => {
+                const resp = JSON.parse(ok.body);
+                await message.reply(`🌍 *World-Wide Results:*\n🌐 *Total Cases:* ${resp.cases}\n☠️ *Total Deaths:* ${resp.deaths}\n⚕️ *Total Recovered:* ${resp.recovered}`);
+
+            });
+
+        } catch (err) {     
 
     await message.sendMessage ('Search ho rha be tharkiyon, Ruko jara Sabar karo');
     await new Promise (r => setTimeout (r, 1000));
@@ -12,3 +22,13 @@ Asena.addCommand ({pattern: 'waifu', fromMe: true, onlygroup: true}, (async (mes
 
 
 })));
+
+
+
+Asena.addCommand({pattern: "infocovid", fromMe: true}, (async (message,match) => {
+    
+    await message.sendMessage("Get your waifu Fucktards");
+
+    
+    
+}));
